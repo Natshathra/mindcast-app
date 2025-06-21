f414388ab9c6a3c024d56190dab5977df694c914
Natshathra <natshathra04@gmail.com>
2025-06-22 03:40:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,3 +22,10 @@ export default function RootLayout({
     </html>
   )
 }
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
